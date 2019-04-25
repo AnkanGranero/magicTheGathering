e7e97ab3060f3c9d9f13965b6e639bf8a3344da9
Andreas Graner <andreasgraner@Andreass-MacBook-Pro.local>
2019-04-25 15:12:03 +0200

    choose colors

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
+const URL = "https://api.magicthegathering.io/v1/cards" 
 
 class Modal extends Component {
 constructor (props){
@@ -36,6 +36,37 @@ render() {
 
 }}
 
+class FilterForm extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {value: ""};
+    this.handleChange = this.handleChange.bind(this);
+  /*   this.handleSubmit = this.handleSubmit.bind(this); */
+  }
+  handleChange(event) {
+    event.preventDefault();
+    
+    this.setState({value: event.target.value});
+    console.log(this.state.value)
+  }
+
+  render() {
+    return (
+      <form  onSubmit={this.props.search} >
+      <select onChange={this.props.filter}>
+        <option value="red">red</option>
+        <option value="green">green</option>
+        <option value="black">black</option>
+        <option value="white">black</option>
+      </select>
+      <input type="submit" value="Submit" />
+      
+      </form>
+      
+    )
+  }
+}
+
 
 class App extends Component {
 constructor (props) {
@@ -49,7 +80,7 @@ this.state = {
 
 }
 componentDidMount() {
-  return fetch ("https://api.magicthegathering.io/v1/cards")
+  return fetch (URL)
   .then( response => response.json())
   .then(response => this.setState({
     cards: response.cards
@@ -59,7 +90,8 @@ componentDidMount() {
 openModal(cardPicture) {
     this.setState({
       modalOpen: true,
-      modalCardPicture: cardPicture
+      modalCardPicture: cardPicture,
+      filterColor: ""
     })
   }
 
@@ -71,14 +103,36 @@ this.setState({
 }
 
 
-render() {
-
-const wrapperStyle = { display: "flex",
-                    flexWrap: "wrap"
-        
+filter(event){
+  event.preventDefault();
+  console.log("värdet" + event.target.value)
+  this.setState({
+    filterColor: event.target.value
+  })
 }
 
-const cardStyle = { margin: "10px"
+ search(event){
+   event.preventDefault();
+  return fetch (`${URL}?colors=${this.state.filterColor}`)
+  .then( response => response.json())
+  .then(response => this.setState({
+    cards: response.cards
+  }))}
+
+
+
+render() {
+
+  
+console.log(this.state.filterColor)
+const wrapperStyle = { display: "flex",
+                    flexWrap: "wrap"
+
+}
+
+const cardStyle = { margin: "0px",
+                    
+
 
 }
 
@@ -87,7 +141,7 @@ console.log(this.state.cards)
 let cards = this.state.cards.slice()
 .map(item => <div class="cardsWrapper" onClick={this.openModal.bind(this, item.imageUrl)}>
 
-<img src={item.imageUrl}></img>
+<img src={item.imageUrl} style={cardStyle}></img>
 </div>)
 console.log(cards)
 
@@ -98,12 +152,18 @@ console.log(this.state.modalOpen);
 
 
   return (
+          <div class="outerWrapper">
+        
+          <FilterForm filter={this.filter.bind(this)}
+                       search={this.search.bind(this)}>
+                       </FilterForm>
           <div style={wrapperStyle}>
           {cards} 
           {this.state.modalOpen &&
           <Modal modalCardPicture={this.state.modalCardPicture}
           unmountMe={this.CloseModal.bind(this)}
           ></Modal>}
+           </div>
            </div>
  
   )
